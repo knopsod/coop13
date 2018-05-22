e31486c5a3bf094a0776472cee944555144edeb8
Noppadon Sodram <knopsod@gmail.com>
2018-05-23 02:16:08 +0700

    Nice form

--- client/components/peoples/PeopleEditor.js
+++ client/components/peoples/PeopleEditor.js
@@ -9,18 +9,35 @@ class PeopleEditor extends Component {
     e.preventDefault();
     this.props.history.push("/");
   }
-
+  handleCancel(e) {
+    e.preventDefault();
+    this.props.history.push("/");
+  }
   render() {
     return (
       <div>
         <Header />
-        <form onSubmit={this.handleSubmit.bind(this)}>
-          <div className="form-group">
-            <label ref="fullName">Full name:</label>
-            <input type="text" className="form-control" ref="fullName"/>
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+        <div className="container">
+          <h2>Peple info.</h2>
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <div className="form-group">
+              <label>No. :</label>
+              <input type="text" className="form-control" ref="no"/>
+            </div>
+            <div className="form-group">
+              <label>Full name :</label>
+              <input type="text" className="form-control" ref="fullName"/>
+            </div>
+            <div className="form-group">
+              <button type="button" className="btn btn-default"
+                onClick={this.handleCancel.bind(this)}>Cancel</button>
+              <span className="pull-right">
+                <button type="submit" className="btn btn-primary">Submit</button>
+              </span>
+            </div>
+          </form>
+        </div>
+
       </div>
     );
   }
